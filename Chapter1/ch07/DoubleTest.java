e7fa15597764b797b77499d02fd351b724dcd3fd
Domo9610 <54881558+Domo9610@users.noreply.github.com>
2021-03-03 22:44:43 +0900

    Add files via upload

---
+++ Chapter1/ch07/DoubleTest.java
@@ -0,0 +1,13 @@
+package ch07;
+
+public class DoubleTest {
+    public static void main(String[] args) {
+
+        double dnum = 3.14;
+        float fnum = 3.14f; //float 데이터 타입을 저장하기 위해서는 뒤에 f를 붙여햐한다.
+
+        System.out.println("double: " + dnum);
+        System.out.println("float: " + fnum);
+
+    }
+}
